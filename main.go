e9a99475f859f5ead8f97c9229a6019ee579cdc4
Roman Maidanskyi <roman.maidanskyi@avenga.com>
2021-07-27 14:56:28 +0300

    feat: read from input

--- main.go
+++ main.go
@@ -2,6 +2,15 @@ package main
 
 import "fmt"
 
+var (
+	name string
+	age int
+)
+
 func main() {
-	fmt.Println("Hello world!")
+	fmt.Println("Your name:")
+	fmt.Scan(&name)
+	fmt.Println("Age:")
+	fmt.Scan(&age)
+	fmt.Printf("Name: %s, age: %d\n", name, age)
 }
